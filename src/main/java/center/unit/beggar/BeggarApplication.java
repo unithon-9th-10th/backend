407e2e5fa04e3a3de6a19d52fa127f5b4936c34f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-09-03 04:25:51 +0900

    chore : 초기 환경 설정

---
+++ src/main/java/center/unit/beggar/BeggarApplication.java
@@ -0,0 +1,14 @@
+package center.unit.beggar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BeggarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeggarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/center/unit/beggar/BeggarApplicationTests.java
@@ -0,0 +1,13 @@
+package center.unit.beggar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeggarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

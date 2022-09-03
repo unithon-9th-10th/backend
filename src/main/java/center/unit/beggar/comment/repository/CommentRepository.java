13fdeb7005ea1d555fdfa7e8bb982293a19d3419
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2022-09-04 01:17:31 +0900

    feat : 챌린지 랭킹 조회 API (#16)

--- src/main/java/center/unit/beggar/comment/repository/CommentRepository.java
+++ src/main/java/center/unit/beggar/comment/repository/CommentRepository.java
@@ -9,5 +9,6 @@ import center.unit.beggar.comment.model.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 	Optional<Comment> findByMember_MemberIdAndExpense_ExpenseId(Long memberId, Long expenseId);
+	List<Comment> findByExpense_expenseIdIn(List<Long> expenseIds);
 	List<Comment> findByExpense_expenseId(Long expenseId);
 }

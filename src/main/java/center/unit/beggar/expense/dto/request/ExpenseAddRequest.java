0bb80442f4f75f7e979886177a272e60e91e594b
전해성 <seong0428@gmail.com>
2022-09-04 02:21:39 +0900

    feat: 지출 referenceDate 필드 추가 (#17)

--- src/main/java/center/unit/beggar/expense/dto/request/ExpenseAddRequest.java
+++ src/main/java/center/unit/beggar/expense/dto/request/ExpenseAddRequest.java
@@ -4,10 +4,12 @@ import center.unit.beggar.expense.model.ExpenseType;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
 public class ExpenseAddRequest {
     private ExpenseType expenseType;
     private BigDecimal amount;
     private String content;
+    private LocalDate referenceDate;
 }

c69256f0829dd68a1a0786318b744bf0e2f9143f
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2022-09-04 05:27:29 +0900

    fix : member 엔티티에 시간정보 추가 (#28)

--- src/main/java/center/unit/beggar/member/model/Member.java
+++ src/main/java/center/unit/beggar/member/model/Member.java
@@ -5,11 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import center.unit.beggar.common.BaseTimeEntity;
 import lombok.Getter;
 
 @Getter
 @Entity
-public class Member {
+public class Member extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)

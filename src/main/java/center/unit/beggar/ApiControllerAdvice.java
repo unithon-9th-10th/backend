4fdb5de5a8228f4ef83976e47893ca76596d242a
Haeseong Jeon <seong0428@gmail.com>
2022-09-14 19:42:44 +0900

    feat: 에러 로그 추가

--- src/main/java/center/unit/beggar/ApiControllerAdvice.java
+++ src/main/java/center/unit/beggar/ApiControllerAdvice.java
@@ -1,16 +1,19 @@
 package center.unit.beggar;
 
 import center.unit.beggar.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ApiControllerAdvice {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResponse<Object> handleException() {
+    public ApiResponse<Object> handleException(Exception e) {
+        log.error("INTERNAL_SERVER_ERROR", e);
         return ApiResponse.failure(
                 "INTERNAL_SERVER_ERROR",
                 "서버 에러가 발생했습니다. 잠시 후 다시 시도해주세요."

7befdcdb651be73a4c198fd5f7fdbe3969f21b19
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2022-09-26 22:16:51 +0900

    Feature/logging (#35)
    * feat : API 요청, 응답 로그 추가
    
    * fix : APi Log에 method 추가로 로깅

---
+++ src/main/java/center/unit/beggar/config/ApiLogFilter.java
@@ -0,0 +1,56 @@
+package center.unit.beggar.config;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@WebFilter(urlPatterns = "/api/*")
+public class ApiLogFilter extends OncePerRequestFilter {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		log.info("[{}] : {}", request.getMethod(), request.getRequestURI());
+
+		// ContentCachingXXXWrapper -> i/o Stream의 유실을 방지하기 위해 body를 caching하는 wrapper
+		ContentCachingRequestWrapper requestToCache = new ContentCachingRequestWrapper(request);
+		ContentCachingResponseWrapper responseToCache = new ContentCachingResponseWrapper(response);
+
+		filterChain.doFilter(requestToCache, responseToCache);
+
+		String beggarMemberId = requestToCache.getHeader("X-BEGGAR-MEMBER-ID");
+		if (beggarMemberId != null) {
+			log.info("X-BEGGAR-MEMBER-ID : {}", beggarMemberId);
+		}
+
+		log.info("Request Body : {}", objectMapper.readTree(requestToCache.getContentAsByteArray()));
+		log.info("Response Body : {}", objectMapper.readTree(responseToCache.getContentAsByteArray()));
+
+		responseToCache.copyBodyToResponse();
+	}
+
+}
